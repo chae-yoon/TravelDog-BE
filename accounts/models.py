c28492be05746465286e73c999243c00d9f21452
chae-yoon <munimooo9765@gmail.com>
2023-05-12 14:22:08 +0900

    Add user model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+    # 파일 업로드 경로 설정
+    def get_profile_image_upload_path(instance, filename):
+        return f'profile_images/{instance.username}/{filename}'
+    
+    phone_number = models.CharField(max_length=15)
+    profile_image = models.ImageField(upload_to=get_profile_image_upload_path, null=True, blank=True)
--- config/settings.py
+++ config/settings.py
@@ -126,3 +126,8 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'accounts.User'
+
+MEDIA_ROOT = [BASE_DIR / 'media']
+MEDIA_URL = '/media/'
