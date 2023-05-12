8c53fc3a0c9f5b1aefe512781dc111915accd385
chae-yoon <munimooo9765@gmail.com>
2023-05-12 16:23:15 +0900

    Add login api

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,58 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
+from django.contrib.auth import get_user_model
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from django.contrib.auth.hashers import check_password
+from .serializers import UserLoginSerializer
 
 # Create your views here.
+@api_view(['GET', 'POST'])
+def login(request):
+    if request.method == 'GET':
+        return Response(data={
+            "message" : "Hello",
+        })
+    elif request.method == 'POST':
+        username = request.data['username']
+        password = request.data['password']
+
+        User = get_user_model()
+        user = User.objects.filter(username=username).first()
+
+        # 만약 username에 맞는 user가 존재하지 않는다면,
+        if user is None:
+            return Response(
+                {"message": "존재하지 않는 아이디입니다."}, status=status.HTTP_400_BAD_REQUEST
+            )
+
+        # 비밀번호가 틀린 경우,
+        if not check_password(password, user.password):
+            return Response(
+                {"message": "비밀번호가 틀렸습니다."}, status=status.HTTP_400_BAD_REQUEST
+            )
+
+        # user가 맞다면,
+        if user is not None:
+            token = TokenObtainPairSerializer.get_token(user) # refresh 토큰 생성
+            refresh_token = str(token) # refresh 토큰 문자열화
+            access_token = str(token.access_token) # access 토큰 문자열화
+            response = Response(
+                {
+                    "user": UserLoginSerializer(user).data,
+                    "message": "login success",
+                    "jwt_token": {
+                        "access_token": access_token,
+                        "refresh_token": refresh_token
+                    },
+                },
+                status=status.HTTP_200_OK
+            )
+
+            response.set_cookie("access_token", access_token, httponly=True)
+            response.set_cookie("refresh_token", refresh_token, httponly=True)
+            return response
+        else:
+            return Response(
+                {"message": "로그인에 실패하였습니다."}, status=status.HTTP_400_BAD_REQUEST
+            )
